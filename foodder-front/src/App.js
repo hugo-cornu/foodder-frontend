e246cf09111acc4679bd44f7e131ee48196956cc
Hugo Cornu <cornu.hugo.p@gmail.com>
2022-06-30 14:09:31 +0200

    Get all data from google map API to send to the backend

--- foodder-front/src/App.js
+++ foodder-front/src/App.js
@@ -7,18 +7,30 @@ function App() {
 
   const handleSelect = async (value) => {
     const results = await geocodeByAddress(value)
-    console.log("results:", results)
+
+    const adressComponent = results[0].address_components
+    const data = {}
+
+    // get country and cca2 info
+    for (let value of adressComponent) {
+      if (value.types[0].toLowerCase() === "country") {
+        data.country = value.long_name
+        data.cca2 = value.short_name
+      }
+    }
+    // Get city name info
+    for (let value of adressComponent) {
+      if (value.types[0].toLowerCase() === "locality") {
+        data.city = value.long_name
+      }
+    }
+    console.log("data:", data)
     setAddress(value)
   }
 
   // Gonstrain Google Maps API predictions to certain place types
   const searchOptions = {
-    types: [
-      "country",
-      "locality",
-      "sublocality",
-      "administrative_area_level_3",
-    ],
+    types: ["locality", "sublocality", "administrative_area_level_3"],
   }
 
   return (
